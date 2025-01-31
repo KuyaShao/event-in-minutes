7f89f77265d562eba557ab7b2ab8380397538de5
Adrian Ardais <Elmer@JZ28B-114.JZ28B-114>
2025-01-31 08:42:47 +0800

    Ensuring its compatible on netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  nitro: {
+    preset: "netlify",
+  },
   css: ["@/assets/css/main.css"],
   devtools: { enabled: true },
   runtimeConfig: {
